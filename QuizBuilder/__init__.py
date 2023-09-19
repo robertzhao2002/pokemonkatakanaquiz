9014ecbb928d7e3bafa9b27ec89b25b259781896
Robert Zhao <rzhao1234@gmail.com>
2023-09-19 12:18:36 -0400

    Update Teal Mask

--- QuizBuilder/__init__.py
+++ QuizBuilder/__init__.py
@@ -5,14 +5,16 @@ import sys
 
 arguments = sys.argv
 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+)
 headers = {"user-agent": USER_AGENT}
 
 URL = "https://bulbapedia.bulbagarden.net/wiki/List_of_Japanese_Pok%C3%A9mon_names"
 
-TOTAL_POKEMON = 1008
+TOTAL_POKEMON = 1017
 
-if (len(arguments) == 2 or len(arguments) == 3) and arguments[1] == 'update':
+if (len(arguments) == 2 or len(arguments) == 3) and arguments[1] == "update":
     generations_to_update = list(range(1, 10))
     print("Updating ...")
 
@@ -28,26 +30,37 @@ if (len(arguments) == 2 or len(arguments) == 3) and arguments[1] == 'update':
         try:
             for generation_number in generations_to_update:
                 filename_generation = str(generation_number)
-                results = soup.find_all('table', {'class': 'roundy'})[
-                    generation_number-1].findAll('tr', {'style': "background:#FFF"})
+                results = soup.find_all("table", {"class": "roundy"})[
+                    generation_number - 1
+                ].findAll("tr", {"style": "background:#FFF"})
 
-                with open('data/gen'+filename_generation+'.csv', 'w', newline='', encoding='utf-8') as csvfile:
+                with open(
+                    "data/gen" + filename_generation + ".csv",
+                    "w",
+                    newline="",
+                    encoding="utf-8",
+                ) as csvfile:
                     writer = csv.writer(csvfile)
 
                     for row_number, entry in zip(range(len(results)), results):
                         if row_number == 0:
                             writer.writerow(
-                                ["pokedex number", "english name", "katakana", "romaji"])
+                                ["pokedex number", "english name", "katakana", "romaji"]
+                            )
 
-                        pokemon_entry = entry.find_all('td')
+                        pokemon_entry = entry.find_all("td")
 
                         pokemon_number = pokemon_entry[0].get_text().strip()
                         pokemon_english = pokemon_entry[2].get_text().strip()
                         pokemon_katakana = pokemon_entry[3].get_text().strip()
                         pokemon_romaji = pokemon_entry[4].get_text().strip()
 
-                        fields = [pokemon_number, pokemon_english,
-                                  pokemon_katakana, pokemon_romaji]
+                        fields = [
+                            pokemon_number,
+                            pokemon_english,
+                            pokemon_katakana,
+                            pokemon_romaji,
+                        ]
 
                         # print("Pokemon Number:", pokemon_number)
                         # print("English Name:", pokemon_english)
